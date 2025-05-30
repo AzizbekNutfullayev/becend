c6c0d45b22d45e797658bc03faf6dc41226eec35
Nutfullayev <nutfullayevazizbek5@gmail.com>
2025-05-30 19:17:31 +0500

    first commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-
-const {Pool} = require('pg')
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "net working",
-    password: "1234",
-    port: 5432,
-  });
-  
-  module.exports = pool;
+  user: "network",
+  host: "dpg-d0sr6eqli9vc73da5100-a.oregon-postgres.render.com",
+  database: "network_ytvc",
+  password: "iZFzoOYtFXktoGJTLAm4hTRHldci3fkD",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,  
+  },
+});
+
+module.exports = pool;
